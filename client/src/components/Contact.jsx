4201870ef6a8f722f183f66fdd3037b140195228
Maher Hussain <maherhussain6@gmail.com>
2023-08-27 01:09:14 +0200

    feat: show offline users

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import Avatar from "./Avatar";
 // eslint-disable-next-line react/prop-types
-export default function Contact({ username, userId }) {
+export default function Contact({ username, userId, isOnline }) {
   return (
     <div className="flex w-full gap-2 items-center border-b p-2">
-      <Avatar username={username} userId={userId} />
-      <span className=" ">{username}</span>
+      <Avatar username={username} userId={userId} online={isOnline} />
+      <span className="">{username}</span>
     </div>
   );
 }
