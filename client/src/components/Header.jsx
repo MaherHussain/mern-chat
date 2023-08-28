4133908833bf5d00cee14e05431614916c516122
Maher Hussain <maherhussain6@gmail.com>
2023-08-28 23:17:45 +0200

    feat: logout function

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,29 @@
 import React, { useContext } from "react";
-import PropTypes from "prop-types";
 import { UserContext } from "./UserContext";
+import axios from "axios";
+import { useNavigate } from "react-router";
+
 export default function Header() {
-  const { username } = useContext(UserContext);
+  const { username, setUsername, setId } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function logout() {
+    axios.post("/logout").then(() => {
+      setId(null);
+      setUsername(null);
+    });
+    navigate(0);
+  }
+
   return (
     <div className="header flex flex-row p-5 bg-blue-500 text-white">
       <p className="grow ">
-        Hello <span className="font-bold">{username}</span>{" "}
+        Hello <span className="font-bold">{username}</span>
       </p>
       <div className="flex-none w-15">
-        <button className="rounded-sm p-3 hover:text-red-700">Logout</button>
+        <button className="rounded-sm p-3 hover:text-red-700" onClick={logout}>
+          Logout
+        </button>
       </div>
     </div>
   );
