00811d5a87092f5d1e22713a254a29503311156c
Maher Hussain <maherhussain6@gmail.com>
2023-09-07 22:48:05 +0200

    feat: add lint-staged setup

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,245 +1,245 @@
-import React, { useEffect, useState, useContext, useRef } from "react";
-import { UserContext } from "./UserContext";
-import Header from "./Header";
-import Contact from "./Contact";
-import { uniqBy } from "lodash";
-import axios from "axios";
+import React, { useEffect, useState, useContext, useRef } from 'react';
+import { UserContext } from './UserContext';
+import Header from './Header';
+import Contact from './Contact';
+import { uniqBy } from 'lodash';
+import axios from 'axios';
 
 export default function Chat() {
-  const [ws, setWs] = useState(null);
-  const [onlineUsers, setOnlineUsers] = useState([]);
-  const [offlineUsers, setOfflineUsers] = useState([]);
-  const [selectedUserId, setSelectedUserId] = useState(null);
-  const [newMessageText, setNewMessageText] = useState("");
-  const [messages, setMessages] = useState([]);
-  const { id, username } = useContext(UserContext);
+	const [ws, setWs] = useState(null);
+	const [onlineUsers, setOnlineUsers] = useState([]);
+	const [offlineUsers, setOfflineUsers] = useState([]);
+	const [selectedUserId, setSelectedUserId] = useState(null);
+	const [newMessageText, setNewMessageText] = useState('');
+	const [messages, setMessages] = useState([]);
+	const { id } = useContext(UserContext);
 
-  useEffect(() => {
-    connectToWS();
-  }, []);
+	useEffect(() => {
+		connectToWS();
+	}, []);
 
-  function connectToWS() {
-    const ws = new WebSocket(`ws://localhost:3001`);
-    setWs(ws);
-    ws.addEventListener("message", messageHandler);
-    ws.addEventListener("close", () => {
-      setTimeout(() => {
-        connectToWS;
-      }, 1000);
-    });
-  }
+	function connectToWS() {
+		const ws = new WebSocket('ws://localhost:3001');
+		setWs(ws);
+		ws.addEventListener('message', messageHandler);
+		ws.addEventListener('close', () => {
+			setTimeout(() => {
+				connectToWS;
+			}, 1000);
+		});
+	}
 
-  function showOnlineUsers(onlineUsers) {
-    const uniqueUsers = [];
-    const userIds = new Set(); // To keep track of seen user IDs and ensure uniqueness
-    onlineUsers.forEach(({ _id, username }) => {
-      if (!userIds.has(_id)) {
-        userIds.add(_id);
-        uniqueUsers.push({ _id, username, online: true });
-      }
-    });
-    setOnlineUsers(uniqueUsers);
-  }
+	function showOnlineUsers(onlineUsers) {
+		const uniqueUsers = [];
+		const userIds = new Set(); // To keep track of seen user IDs and ensure uniqueness
+		onlineUsers.forEach(({ _id, username }) => {
+			if (!userIds.has(_id)) {
+				userIds.add(_id);
+				uniqueUsers.push({ _id, username, online: true });
+			}
+		});
+		setOnlineUsers(uniqueUsers);
+	}
 
-  function messageHandler(e) {
-    const messageData = JSON.parse(e.data);
-    if ("online" in messageData) {
-      showOnlineUsers(messageData.online);
-    } else if ("text" in messageData) {
-      setMessages((prev) => [...prev, { ...messageData }]);
-    }
-  }
-  function sendMessage(ev) {
-    ev.preventDefault();
-    if (newMessageText.trim() === "") {
-      return;
-    }
-    ws.send(
-      JSON.stringify({
-        recipient: selectedUserId,
-        text: newMessageText,
-      })
-    );
-    setNewMessageText("");
-    setMessages((prev) => [
-      ...prev,
-      {
-        sender: id,
-        recipient: selectedUserId,
-        text: newMessageText,
-        _id: Date.now(),
-      },
-    ]);
-  }
-  const divUnderMessages = useRef(null);
+	function messageHandler(e) {
+		const messageData = JSON.parse(e.data);
+		if ('online' in messageData) {
+			showOnlineUsers(messageData.online);
+		} else if ('text' in messageData) {
+			setMessages((prev) => [...prev, { ...messageData }]);
+		}
+	}
+	function sendMessage(ev) {
+		ev.preventDefault();
+		if (newMessageText.trim() === '') {
+			return;
+		}
+		ws.send(
+			JSON.stringify({
+				recipient: selectedUserId,
+				text: newMessageText,
+			})
+		);
+		setNewMessageText('');
+		setMessages((prev) => [
+			...prev,
+			{
+				sender: id,
+				recipient: selectedUserId,
+				text: newMessageText,
+				_id: Date.now(),
+			},
+		]);
+	}
+	const divUnderMessages = useRef(null);
 
-  // this use effect to do scrolling to last massage
-  useEffect(() => {
-    const div = divUnderMessages.current;
-    if (div) {
-      div.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "start",
-      });
-    }
-  }, [messages]);
+	// this use effect to do scrolling to last massage
+	useEffect(() => {
+		const div = divUnderMessages.current;
+		if (div) {
+			div.scrollIntoView({
+				behavior: 'smooth',
+				block: 'end',
+				inline: 'start',
+			});
+		}
+	}, [messages]);
 
-  useEffect(() => {
-    axios.get("/messages/" + selectedUserId).then((response) => {
-      setMessages(response.data);
-    });
-  }, [selectedUserId]);
+	useEffect(() => {
+		axios.get('/messages/' + selectedUserId).then((response) => {
+			setMessages(response.data);
+		});
+	}, [selectedUserId]);
 
-  useEffect(() => {
-    axios.get("/users").then((response) => {
-      const offlineUsers = response.data
-        .filter((u) => u._id != id)
-        .filter(
-          (p) => !onlineUsers.map((oluser) => oluser._id).includes(p._id)
-        );
-      setOfflineUsers(offlineUsers);
-    });
-  }, [onlineUsers]);
+	useEffect(() => {
+		axios.get('/users').then((response) => {
+			const offlineUsers = response.data
+				.filter((u) => u._id != id)
+				.filter(
+					(p) => !onlineUsers.map((oluser) => oluser._id).includes(p._id)
+				);
+			setOfflineUsers(offlineUsers);
+		});
+	}, [onlineUsers]);
 
-  const allUsers = [...onlineUsers, ...offlineUsers];
+	const allUsers = [...onlineUsers, ...offlineUsers];
 
-  const usersWithoutLoggedinUser = onlineUsers.filter(
-    (user) => user._id !== id
-  );
+	const usersWithoutLoggedinUser = onlineUsers.filter(
+		(user) => user._id !== id
+	);
 
-  const selectedUserName = allUsers.find((user) => user._id === selectedUserId);
+	const selectedUserName = allUsers.find((user) => user._id === selectedUserId);
 
-  // filter messages and get the message with out dupication by loadash package
-  const messagesWithoutDuplicate = uniqBy(messages, "_id");
+	// filter messages and get the message with out dupication by loadash package
+	const messagesWithoutDuplicate = uniqBy(messages, '_id');
 
-  return (
-    <div className="chat flex flex-col h-screen">
-      <Header setWs={() => setWs(null)} />
-      <div className="chat-content flex flex-row h-screen">
-        <div className="contacts w-1/3 bg-white py-5">
-          {usersWithoutLoggedinUser.map((user) => (
-            <div
-              className={
-                "flex cursor-pointer" +
-                "" +
-                (user._id === selectedUserId ? "bg-blue-200" : "")
-              }
-              key={user._id}
-              onClick={() => setSelectedUserId(user._id)}
-            >
-              {user._id === selectedUserId && (
-                <div className="w-1 h-12 bg-blue-500 rounded-r-md"></div>
-              )}
-              <Contact
-                username={user.username}
-                userId={user._id}
-                isOnline={user.online}
-              />
-            </div>
-          ))}
-          {offlineUsers.map((user) => (
-            <div
-              className={
-                "flex cursor-pointer" +
-                "" +
-                (user._id === selectedUserId ? "bg-blue-200" : "")
-              }
-              key={user._id}
-              onClick={() => setSelectedUserId(user._id)}
-            >
-              {user._id === selectedUserId && (
-                <div className="w-1 h-12 bg-blue-500 rounded-r-md"></div>
-              )}
-              <Contact
-                username={user.username}
-                userId={user._id}
-                isOnline={user.online}
-              />
-            </div>
-          ))}
-        </div>
-        <div className="messages flex flex-col w-2/3 px-3 py-1">
-          <div className="flex-grow">
-            {!selectedUserId ? (
-              <div className="text-gray-500">Select a user to start chat </div>
-            ) : (
-              <div className="relative h-full ">
-                <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
-                  <div className="mr-3 pr-3">
-                    {messagesWithoutDuplicate.map((message) => (
-                      <div
-                        className={
-                          message.sender === id ? "text-right" : "text-left"
-                        }
-                        key={message._id}
-                      >
-                        <div className="inline-block">
-                          {message.sender === id ? (
-                            <span className="text-gray-500 block text-right">
-                              Me
-                            </span>
-                          ) : (
-                            <span className="text-gray-500 block text-left text-sm truncate">
-                              {selectedUserName.username}
-                            </span>
-                          )}
-                          <div
-                            className={
-                              "text-left p-2 my-2 rounded-md text-sm " +
-                              (message.sender === id
-                                ? "bg-blue-500 text-white"
-                                : "bg-white text-gray-500")
-                            }
-                          >
-                            <p className="px-3 py-2 ">{message.text}</p>
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                    <div className="mb-2" ref={divUnderMessages}></div>
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-          {selectedUserId && (
-            <div className="self-end w-full z-50">
-              <form onSubmit={sendMessage} className="">
-                <div className="flex gap-2 mb-1 ">
-                  <textarea
-                    onChange={(e) => setNewMessageText(e.target.value)}
-                    value={newMessageText}
-                    type="text"
-                    rows="3"
-                    className="flex-grow px-1 rounded-md  "
-                    style={{ resize: "none", fontSize: "18px" }}
-                  />
-                  <button
-                    className="text-white bg-blue-500 px-3 rounded-md border-sky-500  hover:bg-blue-700"
-                    type="submit"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </form>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='chat flex flex-col h-screen'>
+			<Header setWs={() => setWs(null)} />
+			<div className='chat-content flex flex-row h-screen'>
+				<div className='contacts w-1/3 bg-white py-5'>
+					{usersWithoutLoggedinUser.map((user) => (
+						<div
+							className={
+								'flex cursor-pointer' +
+								'' +
+								(user._id === selectedUserId ? 'bg-blue-200' : '')
+							}
+							key={user._id}
+							onClick={() => setSelectedUserId(user._id)}
+						>
+							{user._id === selectedUserId && (
+								<div className='w-1 h-12 bg-blue-500 rounded-r-md'></div>
+							)}
+							<Contact
+								username={user.username}
+								userId={user._id}
+								isOnline={user.online}
+							/>
+						</div>
+					))}
+					{offlineUsers.map((user) => (
+						<div
+							className={
+								'flex cursor-pointer' +
+								'' +
+								(user._id === selectedUserId ? 'bg-blue-200' : '')
+							}
+							key={user._id}
+							onClick={() => setSelectedUserId(user._id)}
+						>
+							{user._id === selectedUserId && (
+								<div className='w-1 h-12 bg-blue-500 rounded-r-md'></div>
+							)}
+							<Contact
+								username={user.username}
+								userId={user._id}
+								isOnline={user.online}
+							/>
+						</div>
+					))}
+				</div>
+				<div className='messages flex flex-col w-2/3 px-3 py-1'>
+					<div className='flex-grow'>
+						{!selectedUserId ? (
+							<div className='text-gray-500'>Select a user to start chat </div>
+						) : (
+							<div className='relative h-full '>
+								<div className='overflow-y-scroll absolute top-0 left-0 right-0 bottom-2'>
+									<div className='mr-3 pr-3'>
+										{messagesWithoutDuplicate.map((message) => (
+											<div
+												className={
+													message.sender === id ? 'text-right' : 'text-left'
+												}
+												key={message._id}
+											>
+												<div className='inline-block'>
+													{message.sender === id ? (
+														<span className='text-gray-500 block text-right'>
+															Me
+														</span>
+													) : (
+														<span className='text-gray-500 block text-left text-sm truncate'>
+															{selectedUserName.username}
+														</span>
+													)}
+													<div
+														className={
+															'text-left p-2 my-2 rounded-md text-sm ' +
+															(message.sender === id
+																? 'bg-blue-500 text-white'
+																: 'bg-white text-gray-500')
+														}
+													>
+														<p className='px-3 py-2 '>{message.text}</p>
+													</div>
+												</div>
+											</div>
+										))}
+										<div className='mb-2' ref={divUnderMessages}></div>
+									</div>
+								</div>
+							</div>
+						)}
+					</div>
+					{selectedUserId && (
+						<div className='self-end w-full z-50'>
+							<form onSubmit={sendMessage} className=''>
+								<div className='flex gap-2 mb-1 '>
+									<textarea
+										onChange={(e) => setNewMessageText(e.target.value)}
+										value={newMessageText}
+										type='text'
+										rows='3'
+										className='flex-grow px-1 rounded-md  '
+										style={{ resize: 'none', fontSize: '18px' }}
+									/>
+									<button
+										className='text-white bg-blue-500 px-3 rounded-md border-sky-500  hover:bg-blue-700'
+										type='submit'
+									>
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											fill='none'
+											viewBox='0 0 24 24'
+											strokeWidth={1.5}
+											stroke='currentColor'
+											className='w-6 h-6'
+										>
+											<path
+												strokeLinecap='round'
+												strokeLinejoin='round'
+												d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
+											/>
+										</svg>
+									</button>
+								</div>
+							</form>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -1,24 +1,24 @@
-import React, { useContext } from "react";
-import { UserContext } from "./UserContext";
-import { Routes as Router, Route } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
-import Chat from "./Chat";
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+import { Routes as Router, Route } from 'react-router-dom';
+import Register from './Register';
+import Login from './Login';
+import Chat from './Chat';
 
 export default function Routes() {
-  const { username, userEmail } = useContext(UserContext);
-  if (username || userEmail) {
-    return (
-      <div>
-        <Chat />
-      </div>
-    );
-  }
+	const { username, userEmail } = useContext(UserContext);
+	if (username || userEmail) {
+		return (
+			<div>
+				<Chat />
+			</div>
+		);
+	}
 
-  return (
-    <Router>
-      <Route path="/" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-    </Router>
-  );
+	return (
+		<Router>
+			<Route path='/' element={<Register />} />
+			<Route path='/login' element={<Login />} />
+		</Router>
+	);
 }
